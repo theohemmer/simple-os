2c8662fa461563b98956ac8f6a3a3ac9e6a260e4
Théo HEMMER <theo.hemmer@epitech.eu>
2021-08-11 15:35:01 +0200

    Add comments in the GDT to better understand how it works

--- bootloader/gdt.asm
+++ bootloader/gdt.asm
@@ -2,18 +2,43 @@
 
 gdt_start:
 
+; empty selector
 gdt_null:
-dd 0x0
+dd 0x0 0000 0000 0000 0000 0000 0000 0000 0000
 dd 0x0
 
+; base: 0x00
+; limit: 0xF FF FC
+
+; access byte:
+; PRESENT          PRIVILEGE      DESCRIPTOR      EXECUTABLE
+; 1 (mandatory)    00 (kernel)    1 (code/data)   1 (yes)
+; DIRECTION        READ/WRIABLE   ACCESSED
+; 0 (grows down)   1 (readable)   0 (useless)
+
+; flags:
+; GRANULARITY      SIZE                           USELESS
+; 1 (4KiB pages)   1 (32bits protected mode)      11 (useless)
 gdt_code:
-dw 0xffff
-dw 0x0
-db 0x0
-db 10011010b
-db 11001111b
-db 0x0
+dw 0xffff   ; limit: 0-15
+dw 0x0      ; base: 0-15
+db 0x0      ; base: 16-23
+db 10011010b; acces bytes (8 bits)
+db 11001111b; limit:51-48 (4 bits), flags (4 bits)
+db 0x0      ; base: 24-31
 
+; base: 0x00
+; limit: 0xF FF FC
+
+; access byte:
+; PRESENT          PRIVILEGE      DESCRIPTOR      EXECUTABLE
+; 1 (mandatory)    00 (kernel)    1 (code/data)   0 (no)
+; DIRECTION        READ/WRIABLE   ACCESSED
+; 0 (grows down)   1 (writable)   0 (useless)
+
+; flags:
+; GRANULARITY      SIZE                           USELESS
+; 1 (4KiB pages)   1 (32bits protected mode)      11 (useless)
 gdt_data:
 dw 0xffff
 dw 0x0
